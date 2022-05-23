af74b7470fcd1f506998bf6f60b8bfe45f64f861
Kristijan <Kristijan@DESKTOP-G5S3BKQ>
2022-05-23 21:05:55 +0200

    Resolved

--- main.cpp
+++ main.cpp
@@ -6,28 +6,80 @@ int size_dialog::idd() const {
 }
 
 bool size_dialog::on_init_dialog(){
+	set_int(IDC_EDIT1, x);
+	set_int(IDC_EDIT2, y);
 	return true;
 }
 
 bool size_dialog::on_ok(){
+	try {
+		x = get_int(IDC_EDIT1);
+		y = get_int(IDC_EDIT2);
+	}
+	catch(...) {
+		return false;
+	}
 	return true;
 }
 
 
 void main_window::on_paint(HDC hdc){
+	HBRUSH hbr = CreateSolidBrush(color);
+	RECT rect;
+	GetClientRect(*this, &rect);
+	SetMapMode(hdc, MM_ANISOTROPIC);
+	SetViewportExtEx(hdc, rect.right, rect.bottom, 0);
+	SetWindowExtEx(hdc, x, y, 0);
+	for (int i = 0; i < x; i++) {
+		for (int j = (i & 1); j < y; j += 2) {
+			RECT rc = { i, j, i + 1, j + 1 };
+			FillRect(hdc, &rc, hbr);
+		}
+	}
+	DeleteObject(hbr);
 }
+
+
 
 void main_window::on_command(int id){
 	switch(id){
 		case ID_SIZE:
+		{
+			size_dialog sDlg;
+			sDlg.x = x;
+			sDlg.y = y;
+			if (sDlg.do_modal(0, *this) == IDOK) {
+				x = sDlg.x;
+				y = sDlg.y;
+				InvalidateRect(*this, NULL, true);
+			}
 			break;
+		}
 		case ID_COLOR:
+		{
+			COLORREF c[100] = { 0 };
+			CHOOSECOLOR ch_color;
+			ZeroMemory(&ch_color, sizeof ch_color);
+			ch_color.rgbResult = color;
+			ch_color.lpCustColors = c;
+			ch_color.hwndOwner = *this;
+			ch_color.lStructSize = sizeof ch_color;
+			ch_color.Flags = CC_FULLOPEN | CC_RGBINIT;
+
+			if (ChooseColor(&ch_color)) {
+				color = ch_color.rgbResult;
+				InvalidateRect(*this, NULL, true);
+			}
 			break;
-		case ID_EXIT: 
-			DestroyWindow(*this); 
+		}
+		case ID_EXIT: {
+			DestroyWindow(*this);
 			break;
+		}
+			
 	}
 }
+
 
 void main_window::on_destroy(){
 	::PostQuitMessage(0);
--- main.h
+++ main.h
@@ -4,6 +4,9 @@
 #include "nwpdlg.h"
 
 class size_dialog : public vsite::nwp::dialog {
+public:
+	int x;
+	int y;
 protected:
 	int idd() const override;
 	bool on_init_dialog() override;
@@ -15,4 +18,8 @@ protected:
 	void on_paint(HDC hdc) override;
 	void on_command(int id) override;
 	void on_destroy() override;
+public:
+	COLORREF color = 0x00000000;
+	int x = 8;
+	int y = 8;
 };
